b784e1dcfbd2bedd4e090a6b0fa2a955fedf1405
Cheikhdeme <cheikhdeme17@gmail.com>
2024-02-24 22:45:36 +0000

    i

--- src/com/xyz/comptesManagement/Program.java
+++ src/com/xyz/comptesManagement/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		Client client = new Client(11,"Ali","Baba");
 		Compte compte = new Compte(11,"4567123",client);
 		client.setCompte(compte);
-		Operation op1 = new Operation(new LocalDate(2020,03,05)), "VERS", 4000, compte);
+		Operation op1 = new Operation(new LocalDate(2020,03,05), "VERS", 4000, compte);
 		compte.addOperation(op1);
 		
 		Operation op2 = new Operation(new LocalDate(2020,03,07), "VERS", 5000, compte);
@@ -25,6 +25,8 @@ public class Program {
 		
 		compte.addOperation(new Operation(new LocalDate(2020,03,30), "RETR", 3000, compte));
 		
-		compte.addOperation(new Operation(new LocalDate(2020,04,05,), "RETR", 6000, compte));
+		compte.addOperation(new Operation(new LocalDate(2020,04,05), "RETR", 6000, compte));
 		
-System.out.println("Succes!");
+		System.out.println("Succes!");
+	}
+}
